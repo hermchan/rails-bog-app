9bf6da19f0de4f737bae19b28ed8c71c13f9691d
Herman Chan <hermchan89@gmail.com>
2016-04-29 16:59:39 -0700

    created controllers with create method and working new form

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -9,4 +9,16 @@ class CreaturesController < ApplicationController
     @creature = Creature.new
     render :new
   end
+
+  def create
+    creatures_params = params.require(:creature).permit(:name, :description)
+    creature = Creature.new(creatures_params)
+
+    if creature.save
+      redirect_to creatures_path
+    end
+  end
+
+
+
 end
