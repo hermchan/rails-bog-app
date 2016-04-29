f8575335c133ba8291d8fb826d7d94da7791afad
Herman Chan <hermchan89@gmail.com>
2016-04-29 16:24:50 -0700

    removed git in bog_app

---
+++ bog_app/app/controllers/creatures_controller.rb
@@ -0,0 +1,12 @@
+class CreaturesController < ApplicationController
+
+  def index
+    @creatures = Creature.all
+    render :index
+  end
+
+  def new
+    @creature = Creature.new
+    render :new
+  end
+end
