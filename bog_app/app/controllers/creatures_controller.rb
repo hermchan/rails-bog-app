1d7814d393719b1d7151a63eb81820c630701e76
Herman Chan <hermchan89@gmail.com>
2016-04-30 14:57:59 -0700

    revised edit html, removed edit button from index because it was stopping me from updating

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -20,32 +20,33 @@ class CreaturesController < ApplicationController
     creature = Creature.new(creatures_params)
 
     if creature.save
-      redirect_to creatures_path(creature)
+      redirect_to creature_path(creature)
     end
   end
-
-  # def create
-  #    # whitelist params and save them to a variable
-  #    creature_params = params.require(:creature).permit(:name, :description)
-  #
-  #    # create a new creature from `creature_params`
-  #    creature = Creature.new(creature_params)
-  #
-  #    # if creature saves, redirect to route that displays
-  #    # ONLY the newly created creature
-  #    if creature.save
-  #      redirect_to creature_path(creature)
-  #      # redirect_to creature_path(creature) is equivalent to:
-  #      # redirect_to "/creatures/#{creature.id}"
-  #    end
-  # end
 
   def edit
     @creature = Creature.find_by_id(params[:id])
     render :edit
   end
 
+  def update
+       # get the creature id from the url params
+       creature_id = params[:id]
 
+       # use `creature_id` to find the creature in the database
+       # and save it to an instance variable
+       creature = Creature.find_by_id(creature_id)
 
+       # whitelist params and save them to a variable
+       creature_params = params.require(:creature).permit(:name, :description)
+
+       # update the creature
+       creature.update_attributes(creature_params)
+
+       # redirect to show page for the updated creature
+       redirect_to creature_path(creature)
+       # redirect_to creature_path(creature) is equivalent to:
+       # redirect_to "/creatures/#{creature.id}"
+  end
 
 end
