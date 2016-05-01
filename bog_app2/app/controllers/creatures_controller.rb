e1757a00cf4614b8cf8c6f21933fcf8bed116504
Herman Chan <hermchan89@gmail.com>
2016-05-01 16:58:35 -0700

    created delete controller, functional delete button removes single id

--- bog_app2/app/controllers/creatures_controller.rb
+++ bog_app2/app/controllers/creatures_controller.rb
@@ -23,5 +23,27 @@ class CreaturesController < ApplicationController
     render :show
   end
 
+  def edit
+    @creature = Creature.find(params[:id])
+    render :edit
+  end
 
+  def update
+    creature = Creature.find(params[:id])
+    creature_params = params.require(:creature).permit(:name, :description)
+    creature.update_attributes(creature_params)
+
+    if creature.save
+      redirect_to creature_path(creature)
+    end
+  end
+
+  def destroy
+    creature = Creature.find_by_id(params[:id])
+    creature.destroy
+
+    if creature.destroy
+      redirect_to creatures_path
+    end
+  end
 end
