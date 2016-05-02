f71e6fe8a0736ad1508e631ebb1a42f66af8f3f7
Herman Chan <hermchan89@gmail.com>
2016-05-01 23:09:02 -0700

    added edit form, update controller and routes, delete a creature, edit html page

--- bog_app3/app/controllers/creatures_controller.rb
+++ bog_app3/app/controllers/creatures_controller.rb
@@ -5,6 +5,7 @@ class CreaturesController < ApplicationController
   end
 
   def new
+    @creature = Creature.new
     render :new
   end
 
@@ -14,7 +15,36 @@ class CreaturesController < ApplicationController
     creature = Creature.new(creature_params)
 
     if creature.save
-      redirect_to creatures_path
+      redirect_to creature_path(creature)
     end
+  end
+
+  def show
+    @creature = Creature.find_by_id(params[:id])
+    render :show
+  end
+
+  def edit
+    @creature = Creature.find_by_id(params[:id])
+    render :edit
+  end
+
+  def update
+
+    creature = Creature.find_by_id(params[:id])
+
+    creature_params = params.require(:creature).permit(:name, :description)
+
+    creature.update_attributes(creature_params)
+
+    redirect_to creature_path(creature)
+  end
+
+  def destroy
+    creature = Creature.find_by_id(params[:id])
+
+    creature.destroy
+
+    redirect_to creatures_path
   end
 end
