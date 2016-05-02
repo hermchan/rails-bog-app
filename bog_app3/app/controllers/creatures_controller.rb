104bf4e53a95a75dd12d8b469130643e63e5f7c1
Herman Chan <hermchan89@gmail.com>
2016-05-01 20:02:54 -0700

    added create route, create controller

--- bog_app3/app/controllers/creatures_controller.rb
+++ bog_app3/app/controllers/creatures_controller.rb
@@ -4,5 +4,17 @@ class CreaturesController < ApplicationController
     @creature = Creature.all
   end
 
+  def new
+    render :new
+  end
 
+  def create
+    creature_params = params.require(:creature).permit(:name, :description)
+
+    creature = Creature.new(creature_params)
+
+    if creature.save
+      redirect_to creatures_path
+    end
+  end
 end
